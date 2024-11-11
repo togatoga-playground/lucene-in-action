a2763ffb8451ab917b2c0cfa21d06f02f544da10
togatoga <hitoshitogasaki@gmail.com>
2024-11-11 10:55:15 +0900

    WIP

--- src/main/java/org/example/Indexer.java
+++ src/main/java/org/example/Indexer.java
@@ -1,7 +1,54 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
 public class Indexer {
     public static void main(String[] args) throws Exception {
-        System.out.println("Indexer");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Usage: java " + Indexer.class.getName() + " <input dir> <data dir>");
+        }
+        String indexDir = args[0];
+        String dataDir = args[1];
+
+        long start = System.currentTimeMillis();
+        Indexer indexer = new Indexer(indexDir);
+    }
+    private IndexWriter writer;
+    public Indexer(String indexDir) throws IOException {
+        FSDirectory dir = FSDirectory.open(new File(indexDir).toPath());
+        writer = new IndexWriter(dir, new IndexWriterConfig());
+    }
+    public void close() throws IOException {
+        writer.close();
+    }
+    public int index(String dataDir, FileFilter filter) throws Exception {
+        File[] files = new File(dataDir).listFiles();
+        assert files != null;
+        for (File f: files) {
+            if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead() && (filter == null || filter.accept(f))) {
+                indexFile(f);
+            }
+        }
+        return writer.numRamDocs();
+    }
+
+    protected Document getDocument(File f) throws Exception {
+        Document doc = new Document();
+        return doc;
+    }
+
+    private void indexFile(File f) throws Exception {
+        System.out.println("Indexing " + f.getCanonicalPath());
+        Document doc = getDocument(f);
+        writer.addDocument(doc);
     }
 }
