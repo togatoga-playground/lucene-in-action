32cbc0acd128b2261aabc67412fef7966ae9a534
togatoga <hitoshitogasaki@gmail.com>
2024-11-13 09:01:05 +0900

    Impl Indexer.java

--- src/main/java/org/example/Indexer.java
+++ src/main/java/org/example/Indexer.java
@@ -2,14 +2,17 @@ package org.example;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
 
 public class Indexer {
     public static void main(String[] args) throws Exception {
@@ -21,19 +24,31 @@ public class Indexer {
 
         long start = System.currentTimeMillis();
         Indexer indexer = new Indexer(indexDir);
+        int numIndexed;
+        try {
+            numIndexed = indexer.index(dataDir, new TextFilesFilter());
+        } finally {
+            indexer.close();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Indexing " + numIndexed + " files took " + (end - start) + " milliseconds");
     }
-    private IndexWriter writer;
+
+    private final IndexWriter writer;
+
     public Indexer(String indexDir) throws IOException {
         FSDirectory dir = FSDirectory.open(new File(indexDir).toPath());
-        writer = new IndexWriter(dir, new IndexWriterConfig());
+        writer = new IndexWriter(dir, new IndexWriterConfig(new StandardAnalyzer()));
     }
+
     public void close() throws IOException {
         writer.close();
     }
+
     public int index(String dataDir, FileFilter filter) throws Exception {
         File[] files = new File(dataDir).listFiles();
         assert files != null;
-        for (File f: files) {
+        for (File f : files) {
             if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead() && (filter == null || filter.accept(f))) {
                 indexFile(f);
             }
@@ -41,8 +56,20 @@ public class Indexer {
         return writer.numRamDocs();
     }
 
+    private static class TextFilesFilter implements FileFilter {
+        public boolean accept(File path) {
+            return path.getName().toLowerCase().endsWith(".txt");
+        }
+    }
+
     protected Document getDocument(File f) throws Exception {
         Document doc = new Document();
+        // analyzed & stored
+        doc.add(new Field("contents", new FileReader(f), TextField.TYPE_STORED));
+        // NOT analyzed & stored
+        doc.add(new Field("filename", f.getName(), StringField.TYPE_STORED));
+        // NOT analyzed & stored
+        doc.add(new Field("fullpath", f.getCanonicalPath(), StringField.TYPE_STORED));
         return doc;
     }
 
