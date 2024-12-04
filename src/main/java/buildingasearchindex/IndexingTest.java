d5e0c74ea1980b5150ce64a2654106fa9b4e30af
togatoga <hitoshitogasaki@gmail.com>
2024-12-04 11:03:25 +0900

    Update

--- src/main/java/buildingasearchindex/IndexingTest.java
+++ src/main/java/buildingasearchindex/IndexingTest.java
@@ -1,11 +1,18 @@
 package buildingasearchindex;
 
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IndexingTest {
     protected String[] ids = {"1", "2"};
@@ -47,6 +55,17 @@ class IndexingTest {
     private IndexWriter getWriter() throws IOException {
         return new IndexWriter(directory, new IndexWriterConfig(new WhitespaceAnalyzer()));
     }
+    protected long getHitContent(String fieldName, String searchString) throws IOException {
+        IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(directory));
+        QueryParser parser = new QueryParser(fieldName, new WhitespaceAnalyzer());
+        Query query = null;
+        try {
+            query = parser.parse(searchString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return searcher.search(query, 1).totalHits.value();
+    }
 
     @Test
     public void indexWriter() throws IOException {
@@ -61,5 +80,68 @@ class IndexingTest {
             assertEquals(ids.length, reader.maxDoc());
             assertEquals(ids.length, reader.numDocs());
         }
+    }
+
+    @Test
+    public void deleteBeforeCommit() throws IOException {
+        try (IndexWriter writer = getWriter()) {
+            assertEquals(2, writer.getDocStats().numDocs);
+            writer.deleteDocuments(new Term("id", "1"));
+            assertEquals(2, writer.getDocStats().maxDoc);
+            assertEquals(2, writer.getDocStats().numDocs);
+
+        }
+    }
+
+    @Test
+    public void deleteAfterCommit() throws IOException {
+        try (IndexWriter writer = getWriter()) {
+            assertEquals(2, writer.getDocStats().numDocs);
+            writer.deleteDocuments(new Term("id", "1"));
+            writer.commit();
+            assertEquals(1, writer.getDocStats().maxDoc);
+            assertEquals(1, writer.getDocStats().numDocs);
+        }
+    }
+
+    @Test
+    public void deleteAfterFlush() throws IOException {
+        IndexWriter writer = getWriter();
+        assertEquals(2, writer.getDocStats().numDocs);
+        writer.deleteDocuments(new Term("id", "1"));
+        writer.flush();
+        assertEquals(2, writer.getDocStats().maxDoc);
+        assertEquals(1, writer.getDocStats().numDocs);
+
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopDocs hits = searcher.search(new TermQuery(new Term("id", "1")), 1);
+        assertEquals(1, hits.totalHits.value());
+
+        // commit the changes
+        writer.commit();
+        reader = DirectoryReader.open(directory);
+        searcher = new IndexSearcher(reader);
+        hits = searcher.search(new TermQuery(new Term("id", "1")), 1);
+        assertEquals(0, hits.totalHits.value());
+    }
+
+
+    @Test
+    public void update() throws IOException {
+        assertEquals(1, getHitContent("city", "Amsterdam"));
+
+        IndexWriter writer = getWriter();
+        Document doc = new Document();
+        doc.add(new StringField("id", "1", Field.Store.YES));
+        doc.add(new StoredField("country", "Netherlands"));
+        doc.add(new TextField("contents", "Den Haag has a lot of museums", Field.Store.NO));
+        doc.add(new TextField("city", "Den Haag", Field.Store.YES));
+        writer.updateDocument(new Term("id", "1"), doc);
+        writer.commit();
+
+        assertEquals(1, getHitContent("id", "1"));
+        assertEquals(0, getHitContent("city", "Amsterdam"));
+        assertEquals(1, getHitContent("city", "Den Haag"));
     }
 }
